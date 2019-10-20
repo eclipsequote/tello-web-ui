4cfd79d2e809eec1a047aa51ae373fbb3b0747b1
thishandp7 <thishandp7@gmail.com>
2019-10-19 23:31:51 -0500

    executeCommand function implemented

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const wait = require('waait');
 const iohook = require('iohook');
 
 const config = require('./config');
-const commands = require('./commands');
+const commandDelays = require('./commandsDelays');
 
 console.log(`HOST: ${config.telloHost}`);
 console.log(`PORT: ${config.telloPort}`);
@@ -11,15 +11,35 @@ console.log(`PORT: ${config.telloPort}`);
 const tello = dgram.createSocket('udp4');
 tello.bind(config.telloPort);
 
-tello.on('message', message => {
-  console.log(`MSG: ${message}`);
-});
-
 const handleError = (err) => {
   if(err){
     console.log(`ERROR: ${err}`);
   }
 }
 
-tello.send('command', 0, 7, config.telloPort, config.telloHost, handleError);
-tello.send('battery?', 0, 8, config.telloPort, config.telloHost, handleError);
+const commandList = ['takeoff', 'cw 180', 'land'];
+
+const initSDKMode = async () => {
+  tello.send('command', 0, 7, config.telloPort, config.telloHost, handleError);
+  await wait(commandDelays['command']);
+  tello.send('battery?', 0, 8, config.telloPort, config.telloHost, handleError);
+  await wait(commandDelays['battery?']);
+  console.log('SDK mode initialized');
+}
+
+const executeCommands = async(commandList, message) => {
+  await initSDKMode();
+  const currentCommand = async (commandList) => {
+    if(commandList.length === 0) return
+    let command = commandList.shift();//change this to a linkedList
+    console.log(`${command}`);
+    tello.send(command, 0, command.length, config.telloPort, config.telloHost, handleError);
+    await tello.on('message', message => {
+      console.log(`waiting for message....${message}`);
+      currentCommand(commandList);
+    })
+  }
+  currentCommand(commandList);
+}
+
+executeCommands(commandList);
