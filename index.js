9a90dcaa0cd61f7c35321f204f9e2bf9737c6496
thishandp7 <thishandp7@gmail.com>
2019-10-20 00:44:59 -0500

    Added code to parse the tello state data and refactored the code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dgram = require('dgram');
-const wait = require('waait');
 
 const config = require('./config');
+const utils = require('./utils');
 
 console.log(`HOST: ${config.telloHost}`);
 console.log(`PORT: ${config.telloPort}`);
@@ -12,30 +12,16 @@ tello.bind(config.telloPort);
 const telloState = dgram.createSocket('udp4');
 telloState.bind(config.telloStatePort);
 
-const handleError = (err) => {
-  if(err){
-    console.log(`ERROR: ${err}`);
-  }
-}
 
 const commandList = ['takeoff', 'cw 180', 'land'];
 
-const initSDKMode = async () => {
-  tello.send('command', 0, 7, config.telloPort, config.telloHost, handleError);
-  await wait(500);
-
-  tello.send('battery?', 0, 8, config.telloPort, config.telloHost, handleError);
-  await wait(500);
-
-  console.log('SDK mode initialized');
-}
 
 const executeCommands = async(commandList, message) => {
   const currentCommand = async (commandList) => {
     if(commandList.length === 0) return
     let command = commandList.shift();//change this to a linkedList
     console.log(`${command}`);
-    tello.send(command, 0, command.length, config.telloPort, config.telloHost, handleError);
+    tello.send(command, 0, command.length, config.telloPort, config.telloHost, utils.handleError);
     console.log(`waiting for message...`);
     await tello.on('message', message => {
       console.log(`message: ${message}`);
@@ -45,15 +31,13 @@ const executeCommands = async(commandList, message) => {
   currentCommand(commandList);
 }
 
-
-
 const fly = async () => {
-  await initSDKMode();
+  await utils.initSDKMode(tello, config);
   executeCommands(commandList);
 }
 
 fly();
 
 telloState.on('message', message => {
-  console.log(`${message}`);
+  console.log(`${JSON.stringify(utils.parseTelloState(message.toString()))}`);
 })
---
+++ utils.js
@@ -0,0 +1,32 @@
+const wait = require('waait');
+
+const parseTelloState = (state) => {
+  return state.split(';')
+          .map(s => s.split(":"))
+          .reduce((data, [key, val]) => {
+            data[key] = val;
+            return data;
+          }, {});
+};
+
+const handleError = (err) => {
+  if(err){
+    console.log(`ERROR: ${err}`);
+  }
+}
+
+initSDKMode = async (tello, config) => {
+  tello.send('command', 0, 7, config.telloPort, config.telloHost, handleError);
+  await wait(500);
+
+  tello.send('battery?', 0, 8, config.telloPort, config.telloHost, handleError);
+  await wait(500);
+
+  console.log('SDK mode initialized');
+}
+
+module.exports = {
+  parseTelloState,
+  handleError,
+  initSDKMode
+}
